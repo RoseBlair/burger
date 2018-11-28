431a1913f5afb894edcc0278040aa66439fe47c3
Carolyn Bernier <carolynbbernier@gmail.com>
2018-11-27 23:41:05 -0500

    added jawsDB

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ var mysql = require("mysql");
 //connect to specific database
 
 var connection = mysql.createConnection({
-    host: "h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "localhost",
     port: 3306,
-    user: "yg8csp9ef0zuafx1", 
-    password: "txfjdloewz6z0xmr",
-    database: "qbtou606ipthqqgo"
+    user: "root", 
+    password: "Rosebud1984",
+    database: "burgers_db"
 });
 
 connection.connect(function(err) {
