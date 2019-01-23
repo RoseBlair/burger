ddefa42557b363dd9a5d7c269fcbc131b5bf22d5
carolyn <carolynbbernier@gmail.com>
2019-01-22 22:13:36 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -10,13 +10,13 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     // Database is local
-    connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: password,
-        database: 'burgers_db'
-    })
+    // connection = mysql.createConnection({
+    //     port: 3306,
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: password,
+    //     database: 'burgers_db'
+    // })
 };
 
 
